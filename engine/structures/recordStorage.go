618fed0442ee84f8270a492b58cef0e1854dd7b6
Jolvera2001 <jolvera6@stedwards.edu>
2025-03-14 18:41:43 -0500

    adde record interface

--- engine/structures/recordStorage.go
+++ engine/structures/recordStorage.go
@@ -1,10 +1,26 @@
 package structures
 
+// IRecordStorage provides methods for storing and retrieving variable
+// sized records. It abstracts the underlying block storage, allowing the
+// database to work with logical records rather than physical blocks
 type IRecordStorage interface {
+	// Update updates a record
 	Update(recordId uint, data []byte) error
+
+	// Find grabs a record's data
 	Find(recordId uint) ([]byte, error)
+
+	// Create makes a new record
 	CreateEmpty() (uint32, error)
+
+	// Creates a new record with given data
 	CreateWithData(data []byte) (uint32, error)
+
+	// Creates a new record similar to CreateWithData 
+	// but with a generator which makes data after a 
+	// record is allocated
 	CreateWithGenerator(dataGenerator func(uint32) []byte) (uint32, error)
+
+	// Delete deletes a record by its Id
 	Delete(recordId uint32) error
 }
